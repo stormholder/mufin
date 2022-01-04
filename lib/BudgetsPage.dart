a5debaf79f5232c20d73c5f96cd47b7559a40d64
Alexander <dantedmc78@gmail.com>
2022-01-04 23:18:04 +0300

    First commit. Tinkering

---
+++ lib/BudgetsPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BudgetsPage extends StatefulWidget {
+  const BudgetsPage({Key? key}) : super(key: key);
+
+  final String title = "Budgets";
+
+  @override
+  State<StatefulWidget> createState() => _BudgetsState();
+}
+
+class _BudgetsState extends State<BudgetsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Add',
+        child: const Icon(Icons.add),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+      ),
+    );
+  }
+}
---
+++ lib/styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MuFinTheme {
+      static const ColorScheme light = ColorScheme.light(
+          primary: Color(0xFF00687C),
+          primaryVariant: Color(0xFFABEDFF),
+          secondary: Color(0xFF974713),
+          secondaryVariant: Color(0xFFFFDBC8),
+          surface: Color(0xFFDBE4E7),
+          background: Color(0xFFFBFDF8),
+          error: Color(0xFFBA1B1B),
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onSurface: Color(0xFF3F484B),
+          onBackground: Color(0xFF191C1A),
+          onError: Color(0xFFBA1B1B)
+      );
+
+      static const ColorScheme dark = ColorScheme.dark(
+          primary: Color(0xFF56D6F5),
+          primaryVariant: Color(0xFF004E5E),
+          secondary: Color(0xFFFFB68D),
+          secondaryVariant: Color(0xFF783100),
+          surface: Color(0xFF191C1A),
+          background: Color(0xFF191C1A),
+          error: Color(0xFFFFB4A9),
+          onPrimary: Color(0xFF003642),
+          onSecondary: Color(0xFF552100),
+          onSurface: Color(0xFFE1E3DE),
+          onBackground: Color(0xFFE1E3DE),
+          onError: Color(0xFF680003)
+      );
+
+      MuFinTheme();
+}
